cbd2e2ee630014b5817762910b5924c698f0032c
EnriqueIllexas <illescaspace7@gmail.com>
2023-08-27 08:47:57 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const sectionContant = document.querySelector(".section-contact")
 const buttonContanct = document.querySelector(".button-contact")
 const closeSectionContact = document.querySelector(".close-contact")
-
+const buttonAlertContact = document.querySelector(".button-contact-pres")
 buttonContanct.addEventListener("click", abrirSecctionContact);
 closeSectionContact.addEventListener("click", closeSecctionContact);
+buttonAlertContact.addEventListener("click", cargarAlertaContacto);
 
 function abrirSecctionContact(){
     sectionContant.classList.remove("inactive")
 }
 function closeSecctionContact(){
     sectionContant.classList.add("inactive")
+}
+function cargarAlertaContacto(){
+    let timerInterval
+    Swal.fire({
+    title: 'Auto close alert!',
+    html: 'I will close in <b></b> milliseconds.',
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector('b')
+        timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+        }, 100)
+    },
+    willClose: () => {
+        clearInterval(timerInterval)
+    }
+    }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+    }
+    })
 }
 /*
 let displayIdioma = sectionInicioIdiomas.style.display = 'block';
